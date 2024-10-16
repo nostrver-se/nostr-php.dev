52583e2e288e07f17d1cefa2e74f1e3c6f095fc7
Sebastix <info@sebastix.nl>
2024-10-16 15:05:56 +0200

    remove ) to fix the 404

--- content/.vitepress/theme/NostrPHPLayout.vue
+++ content/.vitepress/theme/NostrPHPLayout.vue
@@ -10,7 +10,7 @@ const { Layout } = DefaultTheme
       <footer class="text-center">
         Released under the MIT License. Supported by OpenSats.
         <br />
-        <a href="https://github.com/nostrver-se/nostr-php/tree/main/website)" target="_blank">Source Code</a>
+        <a href="https://github.com/nostrver-se/nostr-php/tree/main/website" target="_blank">Source Code</a>
       </footer>
     </template>
   </Layout>
